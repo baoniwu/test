6d0aa46f466303327b6f8f5e24f6687388599009
baoniwu <baoniwu@gmail.com>
2017-03-12 21:16:04 +0800

    初始化專案

---
+++ test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float input;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("請輸入數字: ");
+
+                try
+                {
+                    input = float.Parse(Console.ReadLine());
+                    Console.WriteLine(input.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
